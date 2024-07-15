aaba625dc9309d3f6e424b00d72e20cb46ad0341
taio-ops <tayopopoola92@gmail.com>
2024-07-15 13:24:36 +0100

    add some content to content page

--- contact.php
+++ contact.php
@@ -4,6 +4,8 @@
     <main>
         <h1>Contact Page</h1>       
     <main>
+        <h1>Our Contact</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro delectus, qui voluptatibus similique eaque corrupti esse possimus facilis itaque harum distinctio doloribus, labore nam minima illum eos cupiditate. Suscipit, animi.</p>
     <?php
     require_once('assets/footer.php');
 ?>        
