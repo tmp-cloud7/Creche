d68ebe6b4b68cdfd21c456e622fad62b4403f331
taio-ops <tayopopoola92@gmail.com>
2024-09-28 14:48:16 +0100

    Modified About Page

--- about.php
+++ about.php
@@ -9,7 +9,7 @@
            <img src="slide-4.jpg" alt="slide"> 
         </div>   
         <div class="details">
-            <h2>Our Vision</h2>
+            <h2>Vision</h2>
             <p>To be the secondary school of first choice for families within Lagos and beyond known as a purposeful and effective seat of learning, a warm and caring community where all attain their academic and personal best within a state of the art campus.</p>
             <h2>Our Mission</h2>
             <p>To establish, manage and administer a model private school in all spheres with a vision for a greater tomorrow, through a broad, balanced and functional education that is all about success in life.</p>
