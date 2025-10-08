179db38585e79410b4911827d65ae4f1a0b8bda5
taio-ops <tayopopoola92@gmail.com>
2025-10-08 15:13:43 +0100

    Removed search from Navbar

--- assets/header.php
+++ assets/header.php
@@ -36,10 +36,10 @@
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="parent_login.php">Parent Login</a></li>
                     </ul>
                 <?php }?>
-            <form class="d-flex" role="search">
+            <!-- <form class="d-flex" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            </form> -->
             </div>
         </div>
     </nav>
